b6d26330cab90390b405bdcc4ba1c65a8cd29391
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-07-06 21:39:22 +0200

    Links added to navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,16 @@
             <h1>MyPortoPage</h1>
         </div>
         <nav>
-            
+            <ul>
+                <li v-for="link in links" :key="link.name">
+                    <router-link :to="link.link">{{link.name}}</router-link>
+                    <ul v-if="link.sublinks">
+                        <li v-for="sublink in link.sublinks" :key="sublink.name">
+                            <router-link :to="sublink.link">{{ sublink.name}}</router-link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </nav>
     </header>
 </template>
@@ -34,8 +43,21 @@ export default {
         text-align: center
     }
     nav{ 
+        position: absolute;
+        width: 100%;
+        top: 100%;
         background-color: rgb(75, 97, 97);
         color: white;
+        text-align: start;
+    }
+    ul {
+        list-style: none;
+        padding:2px 10px;
+    }
+    li {
+        padding: 5px;
+        font-weight:bolder;
+        font-size: 1.3rem
     }
     h1 {
         color: #fff;
