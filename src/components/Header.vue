da3c3bf69828ad96dda0c6068a9c334d74bc87ad
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-07-06 22:39:53 +0200

    Added sublink toggleability

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,10 @@
         <nav>
             <ul>
                 <li v-for="link in links" :key="link.name">
-                    <router-link :to="link.link">{{link.name}}</router-link>
+                    <input v-if="link.sublinks" type="checkbox" id="sublink-toggle-{{link.name}}"/>
+                    <label v-if="link.sublinks" for="sublink-toggle-{{link.name}}">{{link.name}} <span>+</span></label>
+                    <router-link v-if="link.sublinks" class="link-lg" :to="link.link">{{link.name}}</router-link>
+                    <router-link v-else :to="link.link">{{link.name}}</router-link>
                     <ul v-if="link.sublinks">
                         <li v-for="sublink in link.sublinks" :key="sublink.name">
                             <router-link :to="sublink.link">{{ sublink.name}}</router-link>
@@ -55,13 +58,47 @@ export default {
         padding:2px 10px;
     }
     li {
-        padding: 5px;
+        padding: 10px;
         font-weight:bolder;
         font-size: 1.3rem
+    }
+    a{
+        text-decoration: none;
+        color: #fff;
+        font-family: Tourney, monospace;
+        letter-spacing: .2rem;
+    }
+    .link-lg{
+        display: none;
+    }
+    input {
+        display: none;
+    }
+    input ~ ul {
+        display: none;
+    }
+    input[type="checkbox"]:checked ~ ul {
+        display: block;
+    }
+    input[type="checkbox"]:checked + label span { 
+        transform: rotateZ(675deg) scale(1.7);
+    }
+    label{
+        font-family: Tourney, monospace;
+        letter-spacing: .2rem;
+        user-select: none;
+    }
+    span {
+        font-size: 1.4rem;
+        display: inline-block;
+        transform: scale(1.8) rotateZ(0);
+        transition: all .1s
     }
     h1 {
         color: #fff;
         font-weight: bolder;
         font-family: Tourney, monospace;
+        letter-spacing: .2rem;
+        user-select: none;
     }
 </style>
